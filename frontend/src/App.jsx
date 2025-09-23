3c8b27c34f2f3ae7e78ff91625f2722c3cf6a9c9
macar3420 <mustafa.acar@tuni.fi>
2025-09-23 14:32:50 +0300

    frontend Changes, Biome, remove EsLint (#5)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="page">
+      <div className="board">
+        <div className="column">
+          <h3>To Do</h3>
+        </div>
+
+        <div className="column">
+          <h3>In Progress</h3>
+          <ul className="items"></ul>
+        </div>
+
+        <div className="column">
+          <h3>Done</h3>
+          <ul className="items"></ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
